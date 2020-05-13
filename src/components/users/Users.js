3b392d1d8a0c2ac2713542c666bbbaf5e9a82d7d
sakurabei <bei@bllli.cn>
2020-05-13 10:18:13 +0800

    添加用户校验规则 1

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -21,7 +21,50 @@ export default {
         mobile: ""
       },
       // 是否显示添加用户 对话框
-      dialogAddUserFormVisible: true
+      dialogAddUserFormVisible: true,
+      // 添加用户的检验规则
+      rules: {
+        // 用户名
+        username: [
+          // 判断是否有输入内容
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          // 判断是否正确
+          {
+            min: 3,
+            max: 5,
+            message: "输入内容应该在3-5之间",
+            trigger: "blur"
+          }
+        ],
+        // 用户名
+        password: [
+          // 判断是否有输入内容
+          { required: true, message: "请输入密码", trigger: "blur" },
+          // 判断格式是否正确
+          {
+            min: 5,
+            max: 10,
+            message: "输入密码内容应该在5-10之间",
+            trigger: "blur"
+          }
+        ],
+        // 邮箱
+        email: [
+          {
+            pattern: /^[A-Za-z\d]+([-_.][A-Za-z\d]+)*@([A-Za-z\d]+[-.])+[A-Za-z\d]{2,4}$/,
+            message: "格式不正确",
+            trigger: "blur"
+          }
+        ],
+        // 手机
+        mobile: [
+          {
+            pattern: /^1([38][0-9]|4[579]|5[0-3,5-9]|6[6]|7[0135678]|9[89])\d{8}$/,
+            message: "格式不正确",
+            trigger: "blur"
+          }
+        ]
+      }
     };
   },
   created() {
