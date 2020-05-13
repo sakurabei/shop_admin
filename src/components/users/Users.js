ab10a677db65eff4ab71cd26ef38aeaa794bedaa
sakurabei <bei@bllli.cn>
2020-05-13 15:16:15 +0800

    添加用户收尾，收集表单数据，发送请求

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -21,7 +21,7 @@ export default {
         mobile: ""
       },
       // 是否显示添加用户 对话框
-      dialogAddUserFormVisible: true,
+      dialogAddUserFormVisible: false,
       // 添加用户的检验规则
       rules: {
         // 用户名
@@ -51,6 +51,7 @@ export default {
         // 邮箱
         email: [
           {
+            // 邮箱正则
             pattern: /^[A-Za-z\d]+([-_.][A-Za-z\d]+)*@([A-Za-z\d]+[-.])+[A-Za-z\d]{2,4}$/,
             message: "格式不正确",
             trigger: "blur"
@@ -59,6 +60,7 @@ export default {
         // 手机
         mobile: [
           {
+            // 手机正则
             pattern: /^1([38][0-9]|4[579]|5[0-3,5-9]|6[6]|7[0135678]|9[89])\d{8}$/,
             message: "格式不正确",
             trigger: "blur"
@@ -120,9 +122,50 @@ export default {
       // 传递参数 加载当前页的内容
       this.loadUsersData(curPage, this.queryText);
     },
+    // 开始查询内容
     startQuery() {
       console.log(this.queryText);
       this.loadUsersData(1, this.queryText);
+    },
+    // 显示添加用户对话框
+    showAddUserDialog() {
+      this.dialogAddUserFormVisible = true;
+    },
+    //添加用户
+    async addUser() {
+      // 收集表单数据，发送请求
+      // 格式：axios.post(url,data,config)
+      let res = await axios.post(
+        "http://localhost:8888/api/private/v1/users",
+        this.addUserForm,
+        {
+          headers: {
+            Authorization: localStorage.getItem("token")
+          }
+        }
+      );
+      // console.log(res);
+      if (res.data.meta.status === 201) {
+        // 1.关闭对话框
+        this.dialogAddUserFormVisible = false;
+        // 2.重新刷新页面
+        this.loadUsersData();
+        // 3.添加用户成功提示
+        this.$message({
+          message: "添加用户成功",
+          type: "success",
+          duration: 800
+        });
+        // 4. 重置表单
+        // 其实我觉得这个东西可以删除
+        this.$refs.addUserForm.resetFields();
+      } else {
+        // 添加失败
+      }
+    },
+    //监听对话框关闭
+    dialogClosed() {
+      this.$refs.addUserForm.resetFields();
     }
   }
 };
