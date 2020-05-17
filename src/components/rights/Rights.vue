48d01e50f0f41069126e813366bc7ed0c1ef1a30
sakurabei <bei@bllli.cn>
2020-05-17 18:10:58 +0800

    权限列表等级

--- src/components/rights/Rights.vue
+++ src/components/rights/Rights.vue
@@ -4,7 +4,13 @@
     <el-table :data="rightsData" style="width: 100%">
       <el-table-column prop="authName" label="权限名称" width="180"></el-table-column>
       <el-table-column prop="path" label="路径" width="180"></el-table-column>
-      <el-table-column prop="level" label="等级"></el-table-column>
+      <el-table-column label="等级">
+        <template slot-scope="scope">
+          <span v-if="scope.row.level == 0">一级</span>
+          <span v-else-if="scope.row.level == 1">二级</span>
+          <span v-else>三级</span>
+        </template>
+      </el-table-column>
     </el-table>
   </div>
 </template>
