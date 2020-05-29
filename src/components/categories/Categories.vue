b661a2ed31bb011f30231dacc7021bc931c6ca3d
sakurabei <bei@bllli.cn>
2020-05-29 10:51:59 +0800

    添加分类-弹出对话框

--- src/components/categories/Categories.vue
+++ src/components/categories/Categories.vue
@@ -1,27 +1,60 @@
-
 <template>
   <div>
+    <el-button type="success" plain @click="addCat">添加分类</el-button>
     <el-table :data="catData" style="width: 100%">
-      <el-table-tree-column prop="cat_name" label="分类名称" width="220" tree-key="cat_id" parent-key="cat_pid" level-key="cat_level" :index-size='20'></el-table-tree-column>
+      <el-table-tree-column
+        prop="cat_name"
+        label="分类名称"
+        width="220"
+        tree-key="cat_id"
+        parent-key="cat_pid"
+        level-key="cat_level"
+        :index-size="20"
+      ></el-table-tree-column>
       <el-table-column label="是否有效" width="180">
-        <template slot-scope="scope">{{scope.row.cat_deleted?'否':'是'}}</template>
+        <template slot-scope="scope">{{
+          scope.row.cat_deleted ? "否" : "是"
+        }}</template>
       </el-table-column>
       <el-table-column label="层级" prop="cat_level">
         <template slot-scope="scope">
-          <span v-if="scope.row.cat_level ==0">一级</span>
-          <span v-else-if="scope.row.cat_level==1">二级</span>
+          <span v-if="scope.row.cat_level == 0">一级</span>
+          <span v-else-if="scope.row.cat_level == 1">二级</span>
           <span v-else>三级</span>
         </template>
       </el-table-column>
     </el-table>
+    <!-- 第一个对话框：添加分类对话框 -->
+    <el-dialog title="添加分类" :visible.sync="dialogAddCatFormVisible">
+      <el-form :model="addCatForm" label-width="80px">
+        <el-form-item label="分类名称">
+          <el-input v-model="addCatForm.cat_name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="父级名称">
+          <el-select
+            v-model="addCatForm.cat_pid_arr"
+            placeholder="请选择活动区域"
+          >
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogAddCatFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogAddCatFormVisible = false"
+          >确 定</el-button
+        >
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 /* eslint-disable */
-import Vue from 'vue'
-import ElTreeGrid from 'element-tree-grid'
-Vue.component(ElTreeGrid.name,ElTreeGrid)
+import Vue from "vue";
+import ElTreeGrid from "element-tree-grid";
+Vue.component(ElTreeGrid.name, ElTreeGrid);
 export default {
   data() {
     return {
@@ -31,7 +64,12 @@ export default {
           cat_deleted: "是",
           cat_level: "一级"
         }
-      ]
+      ],
+      dialogAddCatFormVisible: false,
+      addCatForm: {
+        cat_name: "",
+        cat_pid_arr: []
+      }
     };
   },
   created() {
@@ -48,10 +86,12 @@ export default {
       });
       console.log(res);
       this.catData = res.data.data.result;
+    },
+    addCat() {
+      this.dialogAddCatFormVisible = true;
     }
   }
 };
 </script>
 
-<style>
-</style>
+<style></style>
